48e243154fdf3746a242d0f6ac6cd55800e31152
robinyeon <robin.yeon@gmail.com>
2023-10-06 15:40:36 +0900

    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'laphoo card',
       theme: ThemeData(
         primarySwatch: Colors.brown
